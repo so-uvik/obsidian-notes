7bab0f00f8500c2f7c2e346bd5630d038500504b
so-uvik <souviksahamay27@gmail.com>
2026-08-07 12:30:48 +0530

    vault backup: 2026-08-07 12:30:48

--- .obsidian.init.lua
+++ .obsidian.init.lua
@@ -3,5 +3,5 @@ vim.g.mapleader = " "
 vim.opt.scrolloff = 8
 vim.opt.textobjects = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.number = "true"
-vim.opt.relativenumber = "true"
+vim.opt.number = true
+vim.opt.relativenumber = true
