72ff0475a501489d3bf7e0e4ef38329ba6295fea
so-uvik <souviksahamay27@gmail.com>
2026-08-08 13:23:29 +0530

    vault backup: 2026-08-08 13:23:29

--- .obsidian.init.lua
+++ .obsidian.init.lua
@@ -7,3 +7,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.keymap.set('n', 'L', ':nexttab<CR>', { desc = 'Next tab' })
 vim.keymap.set('n', 'H', ':prevtab<CR>', { desc = 'Previous tab' })
+-- <leader><Space>: native quick switcher (find/create note)
+vim.keymap.set('n', '<leader><Space>', function()
+  vim.cmd('obcommand switcher:open')
+end, { desc = 'Find or create note' })
